acf25088c9ba3e18192b6c2d1681e4a80cddaf41
Dashundel <steshitc@mail.ru>
2023-05-11 18:03:24 +0300

    Add files via upload

--- HW456/Main.cs
+++ HW456/Main.cs
@@ -8,32 +8,5 @@ public class MainClass
         Console.WriteLine(ph.PhoneNumberWithoutCode());
         
         Lesson5.func();
-        
-        
-        ///////////////////
-        BaseClient client = new BaseClient();
-        BaseClient client2 = new BaseClient();
-        StandartClient standart = new StandartClient();
-        StandartClient standart2 = new StandartClient();
-        standart.AddAccount(new CheckingAccount(12, "2322000", 100));
-        standart.AddAccount(new MetalAccount("Au", 12, 25, "4040000"));
-        standart2.AddAccount(new CheckingAccount(12, "1331000", 500));
-        Console.WriteLine($"Общая сумма: {client.TotalBalace}");
-        standart.AccountList();
-        standart2.AccountList();
-        standart.CompareTo(standart);
-        client.AccountList();
-
-        BaseClient[] compare = new BaseClient[] { standart, standart2 };
-        Array.Sort(compare);
-
-        for (int i = 0; i < compare.Length; i++)
-        {
-            Console.WriteLine(compare[i].TotalBalace);
-        }
-
-        SavingsAccount saving = new SavingsAccount("2332432", 100);
-        client.CloseAccount("0001");
-
     }
 }
